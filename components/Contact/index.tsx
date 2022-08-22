0510ebd7e0208d39728826602b8d03ee9584e894
zohaf <zoha.fard@eatfirst.com>
2022-08-22 17:49:43 +0200

    add social url to the icons

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,21 +1,62 @@
 import FeatherIcon from "feather-icons-react";
+import Link from "next/link";
 import styles from "./Contact.module.scss";
 
-export function Contact() {
-  return (
-    <div className={styles.information}>
-      {/* <p>Built with Next.js, Tailwind and Vercel</p> */}
-      <div className={styles.location}>
-        <FeatherIcon icon="map-pin" size="14" />
-        <p>Currently in Amsterdam, Netherlands</p>
-      </div>
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useEffect, useState } from "react";
 
-      <div className={styles.social}>
-        <FeatherIcon icon="phone" size="16" />
-        <FeatherIcon icon="mail" size="16" />
-        <FeatherIcon icon="linkedin" size="16" />
-        <FeatherIcon icon="github" size="16" />
+const socials = [
+  { socialIcon: "mail", socialUrl: "zohafard@gmail.com" },
+  { socialIcon: "linkedin", socialUrl: "https://www.linkedin.com/in/zohaf/" },
+  { socialIcon: "github", socialUrl: "https://github.com/zohaf" },
+];
+
+export function Contact() {
+  const [hasCopied, setHasCopied] = useState(false);
+  useEffect(() => {
+    if (hasCopied) {
+      setTimeout(() => {
+        setHasCopied(false);
+      }, 3000);
+    }
+  }, [hasCopied]);
+
+  return (
+    <>
+      <div className={styles.information}>
+        <div className={styles.location}>
+          <FeatherIcon icon="map-pin" size="14" />
+          <p>Currently in Amsterdam, Netherlands</p>
+        </div>
+        <div className={styles.social}>
+          {socials.map((social, index) => {
+            if (social.socialIcon === "mail") {
+              return (
+                <>
+                  {hasCopied && (
+                    <div className={styles.notifiction}>email copied!</div>
+                  )}
+
+                  <CopyToClipboard
+                    text={social.socialUrl}
+                    onCopy={() => setHasCopied(true)}
+                  >
+                    <FeatherIcon icon={social.socialIcon} size="16" />
+                  </CopyToClipboard>
+                </>
+              );
+            }
+
+            return (
+              <Link href={social.socialUrl} key={index}>
+                <a target="_blank">
+                  <FeatherIcon icon={social.socialIcon} size="16" />
+                </a>
+              </Link>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
